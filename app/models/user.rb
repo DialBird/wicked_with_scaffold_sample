a34ae0ccdf4ba264991cbc897f3e18361a912db5
Keisuke Taniguchi <rememberthatk@icloud.com>
2018-07-20 05:58:58 +0000

    Userにバリデーションを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ApplicationRecord
   validates :email,                 presence: true
-  validates :password,              presence: true
-  validates :password_confirmation, presence: true
-  validates :zip,                   presence: true
-  validates :address1,              presence: true
-  validates :address2,              presence: true
+  validates :password,              presence: true, if: :password_confirmation_or_more?
+  validates :password_confirmation, presence: true, if: :password_confirmation_or_more?
+  validates :zip,                   presence: true, if: :set_address_or_more?
+  validates :address1,              presence: true, if: :set_address_or_more?
+  validates :address2,              presence: true, if: :set_address_or_more?
+
+  private
+
+  def password_confirmation_or_more?
+    return false if status.nil?
+    1 <= status
+  end
+
+  def set_address_or_more?
+    return false if status.nil?
+    2 <= status
+  end
 end
