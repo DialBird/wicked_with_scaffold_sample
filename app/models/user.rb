810b1d777fbbedc501bf30190fa5f04bc5dee9ac
Keisuke Taniguchi <rememberthatk@icloud.com>
2018-07-20 01:33:44 +0000

    Userモデルのバリデーションを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :email,                 presence: true
+  validates :password,              presence: true
+  validates :password_confirmation, presence: true
+  validates :zip,                   presence: true
+  validates :address1,              presence: true
+  validates :address2,              presence: true
 end
